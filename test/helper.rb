c9c0f65c7bec256d833a7bfd4578a1985c0411c2
Bill Dueber <bill@dueber.com>
2010-02-06 14:56:29 -0500

    Initial commit to jruby_streaming_update_solr_server.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'jruby_streaming_update_solr_server'
+
+class Test::Unit::TestCase
+end
