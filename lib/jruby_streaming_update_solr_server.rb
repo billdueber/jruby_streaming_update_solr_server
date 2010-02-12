a7e3531329554348df865ce178176acee7037165
Bill Dueber <bill@dueber.com>
2010-02-12 01:03:41 -0500

    Added tests

--- lib/jruby_streaming_update_solr_server.rb
+++ lib/jruby_streaming_update_solr_server.rb
@@ -1,18 +1,88 @@
+if not defined? JRUBY_VERSION
+  raise "jruby streaming update solr server only runs under jruby"
+end
+
+# Load .jar files locally if they haven't already been pulled in.
 begin
-  jardir = File.join(File.dirname(__FILE__), '..', 'jars')
-  Dir.glob("#{jardir}/*.jar") do |x|
-    puts "Requiring #{x}"
-    require x
-  end  
   include_class Java::OrgApacheSolrClientSolrjImpl::StreamingUpdateSolrServer
   include_class Java::OrgApacheSolrCommon::SolrInputDocument
 rescue NameError  => e
-  raise LoadError.new "StreamingUpdateSolrServer only runs under jruby"
+  jardir = File.join(File.dirname(__FILE__), '..', 'jars')
+  Dir.glob("#{jardir}/*.jar") do |x|
+    require x
+  end  
+  retry
 end
 
+# Sugar on top of the org.apache.solr.client.solr.impl.StreamingUpdateSolrServer
+# 
+# Note that several important methods, new and commit, are direct from the java and hence
+# not represented here where I'm just opening up the class to add some sugar. Full documentation
+# for the raw java methods is available at 
+# http://lucene.apache.org/solr/api/org/apache/solr/client/solrj/impl/StreamingUpdateSolrServer.html
+#
+# A quick look at important java methods you can call:
+# 
+# <b>suss = StreamingUpdateSolrServer.new(solrURL, queueSize, numberOfThreads)</b>
+#   The constructor.
+#
+#   [String] solrURL The URL to your solr instance (i.e., http://solr-machine:port/solr)
+#   [Integer] queueSize The size of the queue from which consumer threads will pull
+#     documents ready to be added to Solr and actually do the sending.
+#   [Integer] numberOfThreads The number of consumer threads to do the sending-to-Solr
+# 
+# <b>suss.commit</b>
+#   Send the commit to the solr server
+#   
+# <b>suss.optimize</b>
+#   Send the optimize commnd to the Solr server
+#
+# <b>suss.deleteById(id)</b>
+#
+# <b>suss.deleteById([id1, id2, id3, ...])</b>
+#   Delete the given ID or IDs
+#
+# <b>suss.deleteByQuery(query)</b>
+#   Delete everything that matches +query+
+#   [String] query A valid solr query. Everything that matches will be deleted. So, you can ditch
+#     it all by sending, e.g., '*:*'
+#
+# @author Bill Dueber
 
 class  StreamingUpdateSolrServer
-  alias_method :susadd, :add 
+
+  # Hang onto the java #add for internal use
+  alias_method :sussadd, :add 
+  
+  # Add a document to the SUSS 
+  # @param [SolrInputDocument, #each_pair] doc The SolrInputDocument or hash (or hash-like object
+  # that responds to #each_pair) to add. The latter must be of the form solrfield => value or
+  # solrfield => [list, of, values]. They keys can be either symbols or strings.
+  #
+  # @example Create and add a SolrInputDocument
+  #   url = 'http://solrmachine:port/solr' # URL to solr
+  #   queuesize = 10 # Size of producer cache 
+  #   threads = 2 # Number of consumer threads to push docs from queue to solr
+  #
+  #   suss = StreamingUpdateSolrServer.new(url,queuesize,threads)
+  #
+  #   doc = SolrInputDocument.new
+  #   doc << ['title', 'This is the title']
+  #   doc << ['id', 1]
+  #   suss.add doc  # or suss << doc
+  #   # repeat as desired
+  #   suss.commit
+  #
+  # @example Create and add as a hash
+  #   # The "hash" just needs to be an object that responds to each_pair with field,value(s)
+  #   suss = StreamingUpdateSolrServer.new(url,queuesize,threads)
+  #   doc = {}
+  #   doc['title'] = This is the title'
+  #   doc[:id] = 1 # Can also take symbols instead of strings if you like
+  #   doc[:author] = ['Bill', 'Mike']
+  #   suss << doc
+  #   # repeat as desired
+  #   suss.commit
   
   def add doc
     if doc.is_a? org.apache.solr.common.SolrInputDocument
@@ -22,9 +92,9 @@ class  StreamingUpdateSolrServer
       doc.each_pair do |f,v|
         newdoc << [f,v]
       end
-      susadd newdoc
+      sussadd newdoc
     else
-      puts "ERROR: Need to pass either a org.apache.solr.common.SolrInputDocument or a hash"
+      raise ArgumentError "Need to pass either an org.apache.solr.common.SolrInputDocument or a hash"
     end
   end
   
@@ -32,8 +102,25 @@ class  StreamingUpdateSolrServer
   
 end
 
+# Add some sugar to the SolrInputDocument
+#
+# @author Bill Dueber
 
 class SolrInputDocument
+  
+  # Add a field and value(s) to the document. This is strictly additive; nothing is replaced or removed
+  #
+  # @param [Array<Symbol, String>] fv A two-element array of the form [field, value] or [field, [value1, value2, ...]]
+  # The field name (i.e., fv[0]) is the Solr field name as specified in schema.xml and
+  # must be either a string or a symbol
+  # @return [Array<String>] the list of current values for the field in fv[0]
+  #
+  # @example Add some fields
+  #  doc = SolrInputDocument.new
+  #  doc << ['title', 'Mein Kopf'] #=> ['Mein Kopf']
+  #  doc << ['title', 'My Head!']  #=> ['Mein Kopf', 'My Head!']
+  #  doc << ['author', ['Bill', 'Mike', 'Molly']] #=> ['Bill', 'Mike', 'Molly']
+  
   def << fv
     field = fv[0]
     value = fv[1]
@@ -50,6 +137,14 @@ class SolrInputDocument
     self[field]
   end
   
+  
+  # Get a list of the currently-set values for the passed field
+  #
+  # Note that this will always return either nil (not found) or an array, even of one element
+  #
+  # @param [String, Symbol] field The field whose values you want (as String or Symbol)
+  # @return [Array<String>] An array of values (or nil on not found)
+  #
   def [] field
     if field.is_a?(Symbol)
       field = field.to_s
@@ -61,9 +156,24 @@ class SolrInputDocument
     if v.class == Java::JavaUtil::ArrayList
       return v.to_a
     else 
-      return v
+      return [v]
     end
   end
+  
+  # Set the value(s) for the given field, destroying any values that were already in there
+  #
+  # Note that this is destructive; see #<< to add multiple values to a field
+  #
+  # @param [String, Symbol] field The solr field you're setting the value of
+  # @param [String, Array<String>] value The value or array of values to set
+  # @return [Array<String>] The list of values (i.e., either +value+ or +[value]+)
+  #
+  # @example
+  #  doc = SolrInputDocument.new
+  #  doc[:id] = 1 #=> [1]
+  #  doc[:author] = 'Mike' #=> ['Mike']
+  #  doc[:author] = 'Bill' #=> ['Bill']
+  #  doc[:author] #=> ['Bill']
   
   def []= field, value
     if field.is_a?(Symbol)
@@ -73,14 +183,33 @@ class SolrInputDocument
     self[field]
   end
   
+  
+  # Add keys and values from a hash or hash-like object to the document without removing any
+  # already-added values.
+  #
+  # @param [#each_pair] h A set of field=>value pairs, probably in a Hash. Can be either 
+  # field=>value or field=>[list,of,values]
+  #
+  # @example Merge a hash into an existing document
+  #   doc = SolrInputDocument.new
+  #   doc << [:author, 'Bill']
+  #   h = {}
+  #   h['author'] = 'Mike'
+  #   h['id'] = 1
+  #   h[:copies] = ['Grad reference', 'Long-term storage']
+  #   doc.merge! h
+  #   doc[:id] #=> 1
+  #   doc[:author] #=> ['Bill', 'Mike']
+  #   doc[:copies] #=> ['Grad reference', 'Long-term storage']
+  
   def merge! h
-    # throw an error unless h.respond_to?(:each_pair)
+    unless h.respond_to? :each_pair
+      raise ArgumentError, "Argument must respond to #each_pair"
+    end
     h.each_pair do |k,v|
       self << [k,v]
     end
   end
 end
-
-
 
 
--- test/test_jruby_streaming_update_solr_server.rb
+++ test/test_jruby_streaming_update_solr_server.rb
@@ -1,7 +1,39 @@
 require 'helper'
 
 class TestJrubyStreamingUpdateSolrServer < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "Write some tests, but don't know how to mock up a solr server" do
+    assert_equal 1, 1
   end
+
+  should "Report nil for a document that doesn't include a field" do 
+    doc = SolrInputDocument.new
+    assert_equal nil, doc[:notinthere]
+  end
+  
+  should "Return single and multiple values in arrays" do
+    doc = SolrInputDocument.new
+    doc << [:id, 1]
+    assert_equal [1], doc[:id]
+    doc << [:id, 2]
+    assert_equal [1,2], doc[:id]
+  end
+  
+  should "Add items in hash via merge!" do
+    doc = SolrInputDocument.new
+    doc << [:id, 1]
+    h = {:id => 2, :name => 'Bill'}
+    doc.merge! h
+    assert_equal [1,2], doc[:id]
+    assert_equal ['Bill'], doc[:name]
+  end
+  
+  should "Destroy existing items via []=" do
+    doc = SolrInputDocument.new
+    doc[:id] = 1
+    assert_equal [1], doc[:id]
+    doc[:id] = 2
+    assert_equal [2], doc[:id]
+  end    
+    
+
 end
